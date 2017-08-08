635cc09629922f724adcc8894c89a04edb51bc6f
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-08-08 19:34:27 -0300

    Pacote de teste e Ovelha teste

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+
+public class Main {
+	Simulador s = new Simulador(40,40);
+	SimuladorTela st = new SimuladorTela(40,50);
+	st.simuladorUmaEtapa();
+}
---
+++ src/teste/OvelhaTest.java
@@ -0,0 +1,99 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class OvelhaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testOvelha() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCorre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEstaViva() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMorte() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLocalizacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
