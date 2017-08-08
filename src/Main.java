7472c542a3dd5bf0891ed58d21c573ba174c4149
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-08-08 19:51:01 -0300

    Teste de eclipse

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		Simulador s = new Simulador();
 		s.simulacaoUmaEtapa();
-		SimuladorTela st = new SimuladorTela(40, 40);
+		SimuladorTela st = new SimuladorTela(400, 40);
 	}
 }
