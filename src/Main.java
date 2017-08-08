b58cecc9acc41174b9ffd203e614fc26d5f1eea5
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-08-08 19:50:13 -0300

    huehfdziujds

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
-	Simulador s = new Simulador(40,40);
-	SimuladorTela st = new SimuladorTela(40,50);
-	st.simuladorUmaEtapa();
+	public static void main(String[] args) {
+		Simulador s = new Simulador();
+		s.simulacaoUmaEtapa();
+		SimuladorTela st = new SimuladorTela(40, 40);
+	}
 }
