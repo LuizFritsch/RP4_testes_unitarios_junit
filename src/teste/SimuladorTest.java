8d625e9b25473e9b68c4b63597e7c7d9209cd4e1
Unknown <guilthys@gmail.com>
2017-08-08 23:05:53 -0300

    Iniciando testes em simulador

---
+++ src/teste/SimuladorTest.java
@@ -0,0 +1,39 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimuladorTest {
+
+	@Test
+	public void testSimulador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSimuladorIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testExecutaLongaSimulacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSimulacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSimulacaoUmaEtapa() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRedefine() {
+		fail("Not yet implemented");
+	}
+
+}
