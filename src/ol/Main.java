b057b01eeb7a472f80e89049bafd4817d988cf13
Unknown <guilthys@gmail.com>
2017-08-09 11:06:40 -0300

    Testes limpa campo / limpa localização ok para intervalo de valore validos - Falta valores invalidos.

--- src/ol/Main.java
+++ src/ol/Main.java
@@ -3,7 +3,7 @@ package ol;
 public class Main {
 	public static void main(String[] args) {
 		Simulador s = new Simulador();
-		s.simulacaoUmaEtapa();
-		SimuladorTela st = new SimuladorTela(400, 40);
+		/*s.simulacaoUmaEtapa();
+		/*SimuladorTela st = new SimuladorTela(100, 100);*/
 	}
 }
---
+++ src/teste/CampoTest.java
@@ -0,0 +1,83 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ol.Campo;
+import ol.Simulador;
+
+
+
+public class CampoTest {
+
+	@Test
+	public void testCampo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLimpa() {		
+		Simulador s = new Simulador(5,5);	  
+		
+		assertNotEquals(null,false);
+			
+		
+	}
+
+	@Test
+	public void testLimpaLocalizacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLugarObjectIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLugarObjectLocalizacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObjectAtLocalizacao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetObjectAtIntInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocalizacaoAdjacenteRandomica() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocalizacoesAdjacentesLivres() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocalizacaoAdjacenteLivre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocalizacoesAdjacentes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProfundidade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetLargura() {
+		fail("Not yet implemented");
+	}
+
+}
