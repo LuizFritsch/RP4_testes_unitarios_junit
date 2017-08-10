31fb5da9956e15a6ed3b7053f1b6ee69e432827c
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-08-10 17:01:44 -0300

    Falha tentativa de criar o junit pro caca

--- src/ol/Randomizador.java
+++ src/ol/Randomizador.java
@@ -1,26 +1,23 @@
 package ol;
+
 import java.util.Random;
 
-public class Randomizador
-{
-    private static final int SEED = 1111;
-    private static final Random rand = new Random(SEED);
-    private static final boolean useShared = true;
+public class Randomizador {
+	private static final int SEED = 1111;
+	private static final Random rand = new Random(SEED);
+	private static final boolean useShared = true;
 
-    public static Random getRandom()
-    {
-        if(useShared) {
-            return rand;
-        }
-        else {
-            return new Random();
-        }
-    }
-    
-    public static void reset()
-    {
-        if(useShared) {
-            rand.setSeed(SEED);
-        }
-    }
+	public static Random getRandom() {
+		if (useShared) {
+			return rand;
+		} else {
+			return new Random();
+		}
+	}
+
+	public static void reset() {
+		if (useShared) {
+			rand.setSeed(SEED);
+		}
+	}
 }
