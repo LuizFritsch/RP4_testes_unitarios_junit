b1d2a2a2308df98b52f789b3bb689da9fad1183b
Unknown <guilthys@gmail.com>
2017-08-10 11:44:04 -0300

    Criandas class de teste.

---
+++ src/teste/ContadorTest.java
@@ -0,0 +1,34 @@
+package teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContadorTest {
+
+	@Test
+	public void testContador() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCount() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIncrement() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReset() {
+		fail("Not yet implemented");
+	}
+
+}
