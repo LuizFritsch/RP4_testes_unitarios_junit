e1dc5db99994114eed48db096fdb3f15683b8427
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-08-14 16:12:01 -0300

    tentiva de implementação do junit para provar o erro na Randomizador

--- src/ol/Randomizador.java
+++ src/ol/Randomizador.java
@@ -1,7 +1,10 @@
 package ol;
 
 import java.util.Random;
-
+/*
+ * Iniciando inspeção 14/08/2017 15:56
+ * 
+ */
 public class Randomizador {
 	private static final int SEED = 1111;
 	private static final Random rand = new Random(SEED);
