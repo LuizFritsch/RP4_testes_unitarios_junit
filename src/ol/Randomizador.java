dd343b1977295c749835ce0feeadc8abe19572af
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-08-14 16:12:01 -0300

    Revert "tentiva de implementação do junit para provar o erro na Randomizador"
    This reverts commit e1dc5db99994114eed48db096fdb3f15683b8427.

--- src/ol/Randomizador.java
+++ src/ol/Randomizador.java
@@ -1,10 +1,7 @@
 package ol;
 
 import java.util.Random;
-/*
- * Iniciando inspeção 14/08/2017 15:56
- * 
- */
+
 public class Randomizador {
 	private static final int SEED = 1111;
 	private static final Random rand = new Random(SEED);
--- src/teste/RandomizadorTest.java
+++ src/teste/RandomizadorTest.java
@@ -2,34 +2,18 @@ package teste;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-import java.util.Random;
-
 import org.junit.Ignore;
 import org.junit.Test;
 
 import ol.Randomizador;
 
 public class RandomizadorTest {
+
 	@Ignore
 	@Test
 	public void testGetRandom() {
-		Random r1 = Randomizador.getRandom();
-		Random r2 = Randomizador.getRandom();
-		ArrayList<Double> x1 = new ArrayList();
-		ArrayList<Double> x2 = new ArrayList();
-		for(int x = 0; x<100; x++) {
-			x1.add(r1.nextDouble());
-		}
-		for(int x = 0; x<100; x++) {
-			x2.add(r2.nextDouble());
-		}
-		for(int x = 0; x<100; x++) {
-			System.out.println(x1.get(x));
-			System.out.println(x2.get(x));
-		}
+		Randomizador r = new Randomizador();
 		
-		assertArrayEquals(x1.toArray(), x2.toArray());
 	}
 
 	@Ignore
