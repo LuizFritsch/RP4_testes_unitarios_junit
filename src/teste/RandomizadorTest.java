0f750bffb5ec2a4ac8826ec54070a12dbbf7f12d
Luiz Guilherme Fritsch <gui_lgf@hotmail.com>
2017-08-18 19:45:18 +0000

    RandomizadorTest.java alterado

--- src/teste/RandomizadorTest.java
+++ src/teste/RandomizadorTest.java
@@ -2,6 +2,9 @@ package teste;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -9,17 +12,47 @@ import ol.Randomizador;
 
 public class RandomizadorTest {
 
-	@Ignore
+	
 	@Test
 	public void testGetRandom() {
-		
-		
+		Random rand = Randomizador.getRandom();
+		int random = rand.nextInt(1);
+		assertTrue(random == 0);
+	}
+	/*
+	 * Verificando se gera nmrs iguais
+	 */
+	@Test
+	public void testGetRandomIguais() {
+		Random rand = Randomizador.getRandom();
+		ArrayList<Integer> primeiro = new ArrayList<Integer>();
+		ArrayList<Integer> segundo= new ArrayList<Integer>();
+		int i = 0;
+		while (i < 11) {
+			primeiro.add(rand.nextInt(100));
+			i++;
+		}
+		int j = 0;
+		while (j < 11) {
+			segundo.add(rand.nextInt(100));
+			j++;
+		}
+		int eIgual = 0;
+		for (int k = 0; k < 11; k++) {
+			System.out.println("em "+ k + " da primeira lista" + primeiro.get(k));
+			System.out.println("em "+ k + " da segunda lista" + segundo.get(k));
+			if (primeiro.get(k) == segundo.get(k)) {
+				eIgual++;
+			}
+		}
+		System.out.println(eIgual);
+		assertNotEquals(10, eIgual);
 	}
 
 	@Ignore
 	@Test
 	public void testReset() {
-		fail("Not yet implemented");
+		//assertTrue(true);
 	}
 
 }
