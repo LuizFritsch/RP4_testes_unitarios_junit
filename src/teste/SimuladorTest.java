cb7564b89c908b8a6198f5931af94b360385d0be
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-08-22 19:07:43 -0300

    SimuladorTest Corrigido, usando reflection

--- src/teste/SimuladorTest.java
+++ src/teste/SimuladorTest.java
@@ -1,48 +1,57 @@
 package teste;
+
+import java.awt.List;
 import java.lang.reflect.*;
+import java.util.ArrayList;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import ol.Ovelha;
 import ol.Simulador;
+
 public class SimuladorTest {
 
-	
-   /*
-	*Ao criar um simulador, as dimensoes nao podem ser 0
-	*/
+	/*
+	 * Ao criar um simulador, as dimensoes nao podem ser 0
+	 */
 	@Test
 	public void testSimulador() {
-		Simulador simulador = new Simulador(1,1);
-	}	
-	
-	
-	
+		Simulador simulador = new Simulador(1, 1);
+	}
+
 	@Test
 	/*
 	 * Ao criar um simulador, as dimensoes nao podem ser <= 0,
 	 */
 	public void testSimuladorNegativo() {
-		Simulador simulador = new Simulador(-1,-1);
-	}	
-	
+		Simulador simulador = new Simulador(-1, -1);
+	}
+
 	@Test(expected = Exception.class)
 	/*
-	 *Ao fazer cast, nenhuma exceção é tratada
+	 * Ao fazer cast, nenhuma exceção é tratada
 	 */
 	public void testSimulacaoUmaEtapa() {
 		Simulador simulador = new Simulador();
 		simulador.executaLongaSimulacao();
 	}
-	
+
 	@Test
-	public void testPovoa() {
-		Class c = Class.forName("src/ol/Simulador");
+	public void testPovoa() throws Exception {
+		Class<?> c = Class.forName("Simulador.java");
 		Simulador s = (Simulador) c.newInstance();
-		
-		Method m = c.getDeclaredMethod("povoa", null);
+
+		Method m = c.getDeclaredMethod("povoa");
 		m.setAccessible(true);
-		m.invoke(s, null);
-		
+		m.invoke(s);
+
 	}
-	
-	
+
+	@Test
+	public void simula() {
+		Simulador s = new Simulador(9999, 9999);
+		s.executaLongaSimulacao();		
+	}
 
 }
