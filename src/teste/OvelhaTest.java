5774cb4738f857a8d0cb03367c5cc596bb9dfed4
Luiz Fritsch <fritsch.luiz@gmail.com>
2017-08-25 13:55:57 -0300

    TestProcriaReflection, TestIncrementaIdadeReflection e pequenas modificações que não lembro

--- src/teste/OvelhaTest.java
+++ src/teste/OvelhaTest.java
@@ -3,12 +3,13 @@ package teste;
 import static org.junit.Assert.*;
 
 import java.awt.List;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import org.junit.*;
 import ol.*;
 
 public class OvelhaTest {
-	
+
 	@Test
 	public void testCorre() {
 		ArrayList<Ovelha> teste = new ArrayList<Ovelha>();
@@ -22,9 +23,9 @@ public class OvelhaTest {
 	@Test
 	public void testCorre2() {
 		ArrayList<Ovelha> teste = new ArrayList<Ovelha>();
-		Ovelha abe = new Ovelha(false, new Campo(100, 100), new Localizacao(3,3));
+		Ovelha abe = new Ovelha(false, new Campo(100, 100), new Localizacao(3, 3));
 		int i = 1;
-		while (i < 42) {
+		while (i < 41) {
 			abe.corre(teste);
 			i++;
 		}
@@ -58,7 +59,8 @@ public class OvelhaTest {
 	public void testGetLocalizacao() {
 		Ovelha o = new Ovelha(false, new Campo(50, 50), new Localizacao(49, 49));
 		Localizacao loc = new Localizacao(49, 49);
-		assertTrue(o.getLocalizacao().getLinha() == loc.getLinha() && o.getLocalizacao().getColuna() == loc.getColuna());
+		assertTrue(
+				o.getLocalizacao().getLinha() == loc.getLinha() && o.getLocalizacao().getColuna() == loc.getColuna());
 	}
 
 	@Test
@@ -84,7 +86,35 @@ public class OvelhaTest {
 			}
 		}
 
-		assertTrue(contr>1);
+		assertTrue(contr > 1);
+
+	}
+
+	@Test
+	public void testIncrementaIdade() throws Exception {
+		Ovelha lb = new Ovelha(false, new Campo(50, 50), new Localizacao(1, 1));
+		Method incrementaIdade = lb.getClass().getDeclaredMethod("incrementaIdade");
+		incrementaIdade.setAccessible(true);
+		incrementaIdade.invoke(lb);
+		incrementaIdade.invoke(lb);
+		assertEquals(2, lb.getIdade());
+	}
+
+	@Test
+	public void testProcriaReflection() throws Exception {
+		Ovelha lb = new Ovelha(false, new Campo(50, 50), new Localizacao(1, 1));
+		// Tornar a idade a idade de procriacao
+		Method incrementaIdade = lb.getClass().getDeclaredMethod("incrementaIdade");
+		incrementaIdade.setAccessible(true);
+		int j = 0;
+		while (j < 6) {
+			incrementaIdade.invoke(lb);
+			j++;
+		}
+		Method procria = lb.getClass().getDeclaredMethod("procria");
+		procria.setAccessible(true);
+		int i = (Integer) procria.invoke(lb);
+		assertTrue(i > 0);
 
 	}
 
