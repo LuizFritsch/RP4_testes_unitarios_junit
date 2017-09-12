503f27a9f0553c49ecc994f1aa490fcc3bf54e45
Unknown <guilthys@gmail.com>
2017-09-11 22:03:03 -0300

    removendo o loop

--- src/ol/Main.java
+++ src/ol/Main.java
@@ -6,15 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		Simulador s = new Simulador();
-		int i = 1;
-		int j =0;
 		
-		do {
-		System.out.println("Digite um inteiro");
-		i = scan.nextInt();
-		s.simulacao(j);
-		  j=j+1;
-		  System.out.println(j);
-		}while(i!=0);
 	}
 }
