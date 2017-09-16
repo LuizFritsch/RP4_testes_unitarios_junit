695773c422ea31000fde8a44f19f31ceafa996bf
Guilherme <guilthys@gmail.com>
2017-09-16 08:48:24 -0300

    Pequenas alterações

--- src/ol/Main.java
+++ src/ol/Main.java
@@ -4,6 +4,14 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
+=======
+
+		Simulador s = new Simulador(10,10);
+		s.simulacaoUmaEtapa();
+		s.simulacao(5);
+
+>>>>>>> Stashed changes
 		
 		Simulador s = new Simulador(100,100);
 		s.executaLongaSimulacao();
