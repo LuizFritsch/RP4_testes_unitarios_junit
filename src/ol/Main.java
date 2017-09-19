8fd8cb944c085fac10fbd5651fb1e83d916e1255
Vitor Hugo Maciel dos Santos <vitao375@hotmail.com>
2017-09-19 01:52:45 -0300

    pequenas alterações na main

--- src/ol/Main.java
+++ src/ol/Main.java
@@ -1,19 +1,18 @@
 package ol;
 
-import java.util.Scanner;
+
 
 public class Main {
 	public static void main(String[] args) {
-<<<<<<< Updated upstream
-=======
+
 
 		Simulador s = new Simulador(10,10);
 		s.simulacaoUmaEtapa();
 		s.simulacao(5);
 
->>>>>>> Stashed changes
+
 		
-		Simulador s = new Simulador(100,100);
-		s.executaLongaSimulacao();
+		Simulador s1 = new Simulador(100,100);
+		s1.executaLongaSimulacao();
 	}
 }
